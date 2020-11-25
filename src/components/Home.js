d270fc492e4d20af56986d8af02c80fcf757f222
Jye019 <jihyekim019@gmail.com>
2020-11-25 15:36:39 +0900

    Feat: Home 화면 구성

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import home from '../image/home.jpg'
 
 const Home = () => {
   return (
-    <div className="home content">
-      <img src={home} alt="sample" width="100%" />
+    <div className="home">
+      <div className="contents">
+        <p>안녕하세요,</p>
+        <p style={{ margin: '30px 0' }}>코더가 아닌 개발자를 지향하는</p>
+        <p>김지혜입니다.</p>
+      </div>
     </div>
   )
 }
